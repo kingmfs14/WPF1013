b530fc378339213ffc03407c48a67dbde234c629
kingmfs14 <kingm@fullsail.edu>
2013-10-09 21:04:17 -1000

    Solved DOG YEARS Problem

--- expressions_worksheet/js/script.js
+++ expressions_worksheet/js/script.js
@@ -1,2 +1,12 @@
-// Matthew King 10/7/13 Expressions
+// Matthew King 10/7/13 Expression Worksheet
 
+//DOG YEARS PROBLEM
+// Convert Sparky's age into dog years
+var humanYears = 3;
+var dogYears = 7;
+
+// multiply human years by dog years and get the dog's age
+var dogAge = humanYears * dogYears
+
+// print out Spark's age
+console.log("Sparky is " + humanYears + " human years old which is " + dogAge + " in dog years.");
