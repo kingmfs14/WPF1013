fbcb771d30b4331e850e70c9693b6cabb1a1aa53
kingmfs14 <kingm@fullsail.edu>
2013-10-09 21:09:52 -1000

    Solved SLICE OF PIE PART1

--- expressions_worksheet/js/script.js
+++ expressions_worksheet/js/script.js
@@ -1,7 +1,8 @@
 // Matthew King 10/7/13 Expression Worksheet
 
+
 //DOG YEARS PROBLEM
-// Convert Sparky's age into dog years
+// convert Sparky's age into dog years
 var humanYears = 3;
 var dogYears = 7;
 
@@ -10,3 +11,20 @@ var dogAge = humanYears * dogYears
 
 // print out Spark's age
 console.log("Sparky is " + humanYears + " human years old which is " + dogAge + " in dog years.");
+
+
+//SLICE OF PIE PART1
+// calculate how many slices of pizza each person will get
+var pizzaOrdered = 8;
+var people = 10;
+var slices = 12;
+
+// multiply pizaa times slices divide total number by people
+var peopleSlices = (pizzaOrdered * slices) / people
+
+// print out amount slices each person gets
+console.log("Each person ate " + parseFloat(peopleSlices) + " slices of pizza at the party.")
+
+
+//SLICE OF PIE PART2
+// 
