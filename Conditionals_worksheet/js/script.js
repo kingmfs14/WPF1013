8d10d6153a1602669bf4af3b59cee96a94b6a8c0
kingmfs14 <kingm@fullsail.edu>
2013-10-15 20:58:42 -1000

    completed GRADE LETTER CALCULATOR

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -22,3 +22,36 @@ if(milesLeft > gasStation){
 	//is driver cannot make it to the next station
 	console.log("You only have " + gasTank + " gallons of gas in your tank, better get gas now while you can!");
 }
+
+
+//GRADE LETTER CALCULATOR
+
+//establish variables
+var grade = 97;
+
+//determining conditional
+if(grade >= 95){
+	//if student has gotten an A+
+	console.log("You have a " + grade + "%, which means you have earned a(n) A+ in the class!");
+}else if(grade >= 90 && grade < 95){
+	//if student has gotten an A
+	console.log("You have a " + grade + "%, which means you have earned a(n) A in the class!");
+}else if(grade >= 85 && grade < 90){
+	//if student has gotten an C
+	console.log("You have a " + grade + "%, which means you have earned a(n) B+ in the class!");
+}else if(grade >= 80 && grade < 85){
+	//if student has gotten an C
+	console.log("You have a " + grade + "%, which means you have earned a(n) B in the class!");
+}else if(grade >= 76 && grade < 80){
+	//if student has gotten an C
+	console.log("You have a " + grade + "%, which means you have earned a(n) C+ in the class!");
+}else if(grade >= 73 && grade < 76){
+	//if student has gotten an C
+	console.log("You have a " + grade + "%, which means you have earned a(n) C in the class!");
+}else if(grade >= 70 && grade < 73){
+	//if student has gotten an C
+	console.log("You have a " + grade + "%, which means you have earned a(n) D in the class!");
+}else{
+	//if student has gotten an C
+	console.log("You have a " + grade + "%, which means you have earned a(n) F in the class!");
+}
