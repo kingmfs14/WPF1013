b20288d8ab1cde576f180923c67fbab32fa8136d
kingmfs14 <kingm@fullsail.edu>
2013-10-15 20:40:06 -1000

    completed LAST CHANCE FOR GAS

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -1,27 +1,24 @@
-// Matthew King 10/9/2013 Industrial Expression
+// Matthew King 10/15/2013 Conditional Expression
 
 
-//IRRIGATION PRESSURE
+//LAST CHANCE FOR GAS
 
 // establish variables
-var psiCurrent = prompt ("What is your current functioning PSI? \nOptions: 15, 20, 25, or 30");
-var gpmCurrent = prompt ("What is your current GPM flow from a nearby hose bib?");
-var nozzleSize = prompt ("What size are your nozzles on the station? \nOptions: 5, 8, 10, 12, or 15");
-var fullNozzle = prompt ("How many full nozzles do you have on the station?");
-var halfNozzle = prompt ("How many half nozzles do you have on the station?");
-var quarterNozzle = prompt ("How many quarter nozzles do you have on the station?");
+var mpg = 26;
+var gauge = .55;
+var tankCapacity = 20;
+var gasStation = 200;
 
-// calculate how many GPM are currently being used
+// calculate how many miles until driver runs out of fuel
 
-var nozzleGPM = (nozzleSize / 100) * (psiCurrent / 5);
-var fullGPM =  4 * nozzleGPM * fullNozzle;
-var halfGPM = 2 * nozzleGPM * halfNozzle;
-var quarterGPM = nozzleGPM * quarterNozzle;
-var neededGPM = fullGPM + halfGPM + quarterGPM;
+var gasTank = tankCapacity * gauge;
+var milesLeft =  mpg * gasTank;
 
-//calculate if there is enough GPM to fulfill the needed GPM
-var totalGPM = gpmCurrent - neededGPM;
-var message = "You have " + totalGPM + " GPM left over with the current set up you have. \nIf this is negative you should change nozzle sizes.";
-
-//alert how much GPM is left
-alert (message);
+//determine conditionals
+if(milesLeft > gasStation){
+	//if driver can make it to next station
+	console.log("Yes, you can make it without stopping for gas!");
+}else{
+	//is driver cannot make it to the next station
+	console.log("You only have " + gasTank + " gallons of gas in your tank, better get gas now while you can!");
+}
