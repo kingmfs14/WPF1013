6104c1deee935e333150e0b2c6ce81e940fddf72
kingmfs14 <kingm@fullsail.edu>
2013-10-15 22:41:24 -1000

    determine UTILITY percentage

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -4,7 +4,7 @@
 
 // establish variables
 var income = prompt ("What is your monthly income after taxes?");
-var utlities = prompt ("How much do you spend on utilities, monthly?");
+var utilities = prompt ("How much do you spend on utilities, monthly?");
 var food = prompt ("How much do you spend on food, monthly?");
 var transportation = prompt ("How much do you spend on gas, bus, taxi, metro; monthly?");
 var housing = prompt ("How much do you spend on rent/mortgage, monthly?");
@@ -13,3 +13,5 @@ var personal = prompt ("How much do you spend on personal hygiene, monthly?");
 var debt = prompt ("How much do you spend on paying off debt, monthly?");
 var recreation = prompt ("How much do you spend on recreational activities, monthly?");
 
+//calculations
+var utilityPercent = income * .10;
