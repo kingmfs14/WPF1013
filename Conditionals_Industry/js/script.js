41d065fb9cc54168c22b086301d6f29027a91580
kingmfs14 <kingm@fullsail.edu>
2013-10-16 00:29:14 -1000

    determined PLANTER AREA

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -7,3 +7,6 @@ var width = prompt ("What is the width of the planter? \nIn Inches, please.");
 var length = prompt ("What is your length of the planter? \nIn Inches, please.");
 var trays = prompt ("How many trays of groundcover do you have?");
 var cell = prompt ("What size are your groundcover tray cells? \nOptions: 16, 24, 36, or 48");
+
+// calculations
+var planter = width * length;
