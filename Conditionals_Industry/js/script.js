6ca89308920ae19327af5831a868ccae88eaad6f
kingmfs14 <kingm@fullsail.edu>
2013-10-16 00:27:53 -1000

    gave more guidance to WIDTH & LENGTH prompts

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -3,7 +3,7 @@
 
 
 // establish variables
-var width = prompt ("What is the width of the planter?");
-var length = prompt ("What is your length of the planter?");
+var width = prompt ("What is the width of the planter? \nIn Inches, please.");
+var length = prompt ("What is your length of the planter? \nIn Inches, please.");
 var trays = prompt ("How many trays of groundcover do you have?");
 var cell = prompt ("What size are your groundcover tray cells? \nOptions: 16, 24, 36, or 48");
