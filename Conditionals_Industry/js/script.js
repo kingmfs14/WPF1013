0ca325c8fe85e034b862c9b7099577827cfe2210
kingmfs14 <kingm@fullsail.edu>
2013-10-16 00:38:59 -1000

    created 16 CELL conditional

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -7,7 +7,14 @@ var width = prompt ("What is the width of the planter? \nIn Inches, please.");
 var length = prompt ("What is your length of the planter? \nIn Inches, please.");
 var trays = prompt ("How many trays of groundcover do you have?");
 var cell = prompt ("What size are your groundcover tray cells? \nOptions: 16, 24, 36, or 48");
+var gcNeeded
 
 // calculations
 var planter = width * length;
 var groundcover = trays * cell;
+
+//conditionals
+
+if (cell === 16) {
+	gcNeeded = planter / 18;
+}
