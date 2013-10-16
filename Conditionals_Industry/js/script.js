8d74d40be5551c458ce3574619b6793a580d39bd
kingmfs14 <kingm@fullsail.edu>
2013-10-16 00:25:34 -1000

    prompt user to input CELL AMOUNT

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -6,3 +6,4 @@
 var width = prompt ("What is the width of the planter?");
 var length = prompt ("What is your length of the planter?");
 var trays = prompt ("How many trays of groundcover do you have?");
+var cell = prompt ("What size are your groundcover tray cells? \nOptions: 16, 24, 36, or 48");
