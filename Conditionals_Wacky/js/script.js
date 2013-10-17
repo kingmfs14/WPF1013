d41ce20a11c1d6b5dcdf5d7ad636e0cb46b9e0d6
kingmfs14 <kingm@fullsail.edu>
2013-10-16 17:24:17 -1000

    created FROGGROWTH calculation

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -9,3 +9,5 @@ var currentFrog = prompt ("How many frogs are in one pond?");
 var speed = prompt ("How fast will you drive to crush the frogs? \nIn miles per hour, please.");
 var time = prompt ("How long are you going to be chasing frogs for? \nIn minutes, please.");
 
+//calculations
+var frogGrowth = currentFrog * 5;
