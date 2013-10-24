95544aeabe904007109b601c3051e51772dd47f6
kingmfs14 <kingm@fullsail.edu>
2013-10-23 21:30:23 -1000

    created function for calculating

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -6,3 +6,9 @@
 // Establish variables
 var total = calcCircum(7);
 
+// create function for finding the circumference of a circle
+function calcCircum(radius){
+	var pi = 3.14;
+	var circum = pi * (2 * radius);
+	return circum;
+}
