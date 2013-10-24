790f7e612c87e1e8525b905ce5584cc42445501e
kingmfs14 <kingm@fullsail.edu>
2013-10-23 21:38:12 -1000

    created function for calculating stings

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -21,3 +21,10 @@ console.log("The circumference of the circle is " + total + ".");
 //STUNG!
 // Establish variables
 var total = killAnimal(367);
+
+// create function for determining the amount of bee stings
+function killAnimal(weight){
+	var poison = 8.666666667;
+	var lethal = poison * weight;
+	return lethal;
+}
