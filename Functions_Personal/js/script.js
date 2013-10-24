4345b656c9cad5322c825cbc29dfff76dce8a577
kingmfs14 <kingm@fullsail.edu>
2013-10-24 06:31:03 -1000

    created REHEARSALTIME variable

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -8,3 +8,9 @@ var ceremony = prompt ("Would you like your ceremony video taped?");
 var reception = prompt ("Would you like your reception video taped?");
 var photos = prompt ("Would you like the video to include some photos?");
 
+//create if statements to gather more information
+if (rehearsal === yes) {
+	var rehearseTime = prompt ("How long do you anticipate your rehearsal being? \nIn minutes, please.");
+}else {
+	var rehearseTime = 0;
+}
