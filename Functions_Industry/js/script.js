af7d50bc01627ab4a9f150d226348230617cc906
kingmfs14 <kingm@fullsail.edu>
2013-10-24 16:03:57 -1000

    establish CURRENTFERTILIZE variable

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -7,3 +7,4 @@
 var lawn = prompt ("How many lawn areas do you have that you want to fertilze?");
 var area = prompt ("How large, on average, is each lawn? \nIf one is 200 feet and another 100 feet, then you would enter 150.");
 var spreading = prompt ("How will you be spreading the fertilizer? \nOptions: hand or spreader");
+var currentFertilze = prompt ("How many 50 pound bags of fertilzer do you have?");
