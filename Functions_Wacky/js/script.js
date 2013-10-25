9e542c41d2e40bb45fc5ad5ee9112df3d3f1a5a9
kingmfs14 <kingm@fullsail.edu>
2013-10-24 17:23:34 -1000

    establish RATE variable

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -5,3 +5,4 @@
 //Painting Opponent through the bush
 //establish variables
 var velocity = prompt ("How fast are the paintballs exiting your gun? \nPlease type in between 180 and 320.");
+var rate = prompt ("At what rate are you firing the paintballs at? \nOptions: single, 3, 6, or auto");
