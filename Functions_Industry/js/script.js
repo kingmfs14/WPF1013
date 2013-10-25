510a24c697c2e717191ad76e9c7f3dc4f0db6b63
kingmfs14 <kingm@fullsail.edu>
2013-10-24 16:03:05 -1000

    establish AREA variable

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -5,3 +5,4 @@
 //Lawn Fertilizer Needed
 // Establish variables
 var lawn = prompt ("How many lawn areas do you have that you want to fertilze?");
+var area = prompt ("How large, on average, is each lawn? \nIf one is 200 feet and another 100 feet, then you would enter 150.");
